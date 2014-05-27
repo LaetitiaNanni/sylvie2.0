01b75829e2750bb1a8197828fcee992a6d549a97
AngelZatch <pinbouen.andreas@gmail.com>
2014-05-27 18:43:09 +0200

    mainMenu (WIP)
    Ajout du menu à gauche (PARTIEL)

--- mainMenu.php
+++ mainMenu.php
@@ -1,2 +1,45 @@
+<meta charset="utf-8">
 <p>Ici code du menu principal.</p> 
-   
+
+<div>
+	<nav class="navbar navbar-inverse navbar-embossed" role="navigation">
+		<ul class="nav navbar-nav navbar-left">
+			<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Photo / Vidéo <b class="caret"></b></a>
+				<span class="dropdown-arrow"></span>
+				<ul class="dropdown-menu">
+					<!--A remplacer par des résultats PHP-->
+					<li><a href="#">Appareils photos</a></li>
+					<li><a href="#">Vidéoprojecteurs</a></li>
+					<li><a href="#">Oculus Rift</a></li>
+				</ul>
+			</li>
+			<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Son <b class="caret"></b></a>
+				<span class="dropdown-arrow"></span>
+				<ul class="dropdown-menu">
+					<!--A remplacer par des résultats PHP-->
+					<li><a href="#">Micros</a></li>
+					<li><a href="#">Enregistreurs</a></li>
+				</ul>
+			</li>
+			<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Lumière <b class="caret"></b></a>
+				<span class="dropdown-arrow"></span>
+				<ul class="dropdown-menu">
+					<!--A remplacer par des résultats PHP-->
+					<li><a href="#">Mandarines</a></li>
+					<li><a href="#">Projecteurs Fresnel</a></li>
+				</ul>
+			</li>
+			<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Accessoires <b class="caret"></b></a>
+				<span class="dropdown-arrow"></span>
+				<ul class="dropdown-menu">
+					<!--A remplacer par des résultats PHP-->
+					<li><a href="#">Pieds</a></li>
+					<li><a href="#">Câbles</a></li>
+					<li><a href="#">Cartes Mémoire</a></li>
+					<li><a href="#">Multiprises</a></li>
+					<li><a href="#">Autres</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</div>
