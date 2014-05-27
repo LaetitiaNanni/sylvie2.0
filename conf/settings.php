d3e2ec721dc23f417c383e54f90e1d9bbf4f5120
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-05-27 17:29:05 +0200

    fichiers de config pour bdd

---
+++ conf/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_BASE', 'materimacBDD');
+define('DB_PORT', '');
+
+//define('LIB_PATH', 'lib/');
+//define('ASSETS_PATH', 'assets/');
---
+++ conf/top.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'settings.php';
+
+
+connectDb();
+/*session_start();
+$session=0;
+
+if(isset($_GET["logout"])) {
+	if($_GET["logout"] == 1) {
+		session_destroy();
+		$session=0;
+		header('Location: p_accueil.php'); 
+	}
+}
+if(isset($_SESSION['status'])) {
+	$session=1;
+	// session d'admin
+	if($_SESSION['status']==1) {
+		$session=2;
+	}
+*/
+}
+
+
