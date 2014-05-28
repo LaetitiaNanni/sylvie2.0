ad06a7282d8753e3652afc4fa87e2a17643d2721
JuliePO <pojulie07@gmail.com>
2014-05-28 12:08:24 +0200

    info matos

--- listing.php
+++ listing.php
@@ -1,1 +1,21 @@
 <p>listing materiel</p>
+
+<?php
+
+	$allKits = getAllKits();
+	while($kit = mysql_fetch_object($allKits)) {
+
+		echo ($kit-> label);
+		echo ($kit-> ID_Kit);
+
+		$materiaux = getItemFromKit($kit-> ID_Kit);
+
+		while($matos = mysql_fetch_object($materiaux)) {
+			echo ($matos-> label);
+			echo ($matos-> description);
+			echo ($matos-> brand);
+			echo ($matos-> model);
+		}
+	}
+
+?>
