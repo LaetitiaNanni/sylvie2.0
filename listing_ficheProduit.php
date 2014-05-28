520dce23ddbfd5ee7b05d114f1fa7d279cf25e41
florian <florian.lequertier@free.fr>
2014-05-28 10:57:45 +0200

    Fiche produit

---
+++ listing_ficheProduit.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+	require 'conf/top.php';
+	connectDb();
+
+?>
+
+
+<div id="centre">
+			<div id="video">
+			
+				
+				  <!--[if !IE]> -->
+					<video  class="video-js" controls preload="auto"  width="350px" height="250px"   poster="images/video/poster.jpg" data-setup="{}">
+					  <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.mp4" type="video/mp4">
+					  <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.webm" type="video/webm">
+					</video>
+				  <!-- <![endif]-->
+
+				  <!--[if IE]>
+					<video class="video-js" controls preload="auto" width="620" height="256" poster="http://video-js.zencoder.com/oceans-clip.jpg" data-setup="{}">
+					  <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4'/>
+					  <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm'/>
+					</video>
+				  <![endif]-->
+			
+				
+			</div>
+			
+			<div id="description">
+				<h5> Description :  </h5>
+				<div id="description_image">
+					<img src="images/exaple-image.jpg" alt="exaple-image" class="img-rounded img-responsive">
+				</div>
+				
+				<p>  
+					aaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaa aaaaaaaaa aaaaaaa aaaaaaaaa aaaaaaaaaaaa aaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa
+				</p>
+				<div id="description_disponibilite"> Disponibilité : <span class="fui-check"></span> <span class="fui-cross"></span> </div>
+				<div class="col-xs-10">
+				  <a href="#fakelink" class="btn btn-block btn-lg btn-primary">Ajouter au panier</a>
+				</div>
+				
+			</div>
+			
+			<div id="autre_sets"class="row" >
+					<h4> Autre sets disponnibles : </h4>
+					<div class="col-xs-3">
+						<figure class="vignette">
+							<img src="images/exaple-image.jpg" alt="exaple-image" class="img-rounded img-responsive">
+							<figcaption> titre. </figcaption>
+						</figure>
+					</div>
+					<div class="col-xs-3">
+						<figure class="vignette">
+							<img src="images/exaple-image.jpg" alt="exaple-image" class="img-rounded img-responsive">
+							<figcaption> titre. </figcaption>
+						</figure>
+					</div>
+					<div class="col-xs-3">
+						<figure class="vignette">
+							<img src="images/exaple-image.jpg" alt="exaple-image" class="img-rounded img-responsive">
+							<figcaption> titre. </figcaption>
+						</figure>	
+					</div>
+			
+			</div>
+		</div>
+		
+
+	
