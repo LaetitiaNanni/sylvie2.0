c0fa4db78fcf12c634213693eb3d893e021d856c
JuliePO <pojulie07@gmail.com>
2014-05-28 09:08:15 +0200

    Admin

--- all-variables.php
+++ all-variables.php
@@ -8,19 +8,136 @@
 
 	//Récupérer l'administrateur avec l'id
 	function getAllRent() {
-		$sql = 'SELECT * FROM lending
-				INNER JOIN user ON user.ID_User = lending.ID_User
-				INNER JOIN loan ON loan.ID_Loan = lending.ID_Loan
-				INNER JOIN kit ON kit.ID_Kit = lending.ID_Kit';
+		$sql = 'SELECT * FROM loan';
 		// on envoie la requête à la base de données 
     	$result = mysql_query($sql);
-		$results = NULL ;
+    	return $result;
+		//$results = NULL ;
 		// on utilise mysql_fetch_array pour ranger les résultats de la requête dans un tableau associatif
-		while ($ligne = mysql_fetch_array($result)) {
+		/*while ($ligne = mysql_fetch_object($result)) {
 			$results = $ligne;
 		}
-		return $results;
+		return $results;*/
 	}
 
+	//Récupérer l'administrateur avec l'id
+	function getAllRentNow() {
+		$sql = 'SELECT * FROM loan
+		WHERE loan.ID_State = 3
+		OR loan.ID_State = 1';
+		// on envoie la requête à la base de données 
+    	$result = mysql_query($sql);
+    	return $result;
+		//$results = NULL ;
+		// on utilise mysql_fetch_array pour ranger les résultats de la requête dans un tableau associatif
+		/*while ($ligne = mysql_fetch_object($result)) {
+			$results = $ligne;
+		}
+		return $results;*/
+	}
+
+	function getAllWaitRent() {
+		$sql = 'SELECT * FROM loan
+		WHERE loan.ID_State = 2';
+		// on envoie la requête à la base de données 
+    	$result = mysql_query($sql);
+    	return $result;
+		//$results = NULL ;
+		// on utilise mysql_fetch_array pour ranger les résultats de la requête dans un tableau associatif
+		/*while ($ligne = mysql_fetch_object($result)) {
+			$results = $ligne;
+		}
+		return $results;*/
+	}
+
+	function getAllInfoRent($id_loan) {
+		$sql = 'SELECT * FROM lending
+				LEFT JOIN kit ON kit.ID_Kit = lending.ID_Kit
+				WHERE lending.ID_Loan = '.$id_loan;
+		// on envoie la requête à la base de données 
+    	$result = mysql_query($sql);
+    	return $result;
+	}
+
+
+	/*************************************************************************
+	************  Récupérer tous les matos en fonction du kit  ***************
+	**************************************************************************/
+
+	//Récupérer l'administrateur avec l'id
+	function getItemFromKit($id_kit) {
+		$sql = 'SELECT * FROM item
+				WHERE ID_Kit = '.$id_kit;
+		// on envoie la requête à la base de données 
+    	$result = mysql_query($sql);
+    	return $result;
+		//$results = NULL ;
+		// on utilise mysql_fetch_array pour ranger les résultats de la requête dans un tableau associatif
+		/*while ($ligne = mysql_fetch_object($result)) {
+			$results = $ligne;
+		}
+		return $results;*/
+	}
+
+
+	function getUserByIdLoan($id_loan) {
+		$sql = 'SELECT * FROM user
+				LEFT JOIN lending ON lending.ID_User = user.ID_User
+				LEFT JOIN loan ON lending.ID_Loan = loan.ID_Loan
+				WHERE loan.ID_Loan = '.$id_loan;
+		// on envoie la requête à la base de données 
+    	$result = mysql_query($sql);
+    	return $result;
+	}
+
+
+	function getNameState($id_state) {
+		$sql = 'SELECT name_state FROM state
+				WHERE state.ID_State = '.$id_state;
+		// on envoie la requête à la base de données 
+    	$result = mysql_query($sql);
+    	$results = mysql_fetch_object($result);
+    	return $results;
+    }
+
+
+	function updateState($state, $id_loan) {
+
+		switch($state) {
+			case "emprunte" : 
+				$id_state = 1;
+				break;
+
+			case "en cours" : 
+				$id_state = 2;
+				break;
+
+			case "valide" : 
+				$id_state = 3;
+				break;
+
+			case "annule" : 
+				$id_state = 4;
+				break;
+
+			case "refuse" : 
+				$id_state = 5;
+				break;
+
+			case "retourne" : 
+				$id_state = 6;
+				break;
+		}
+
+		$sql = 'UPDATE loan
+				SET loan.ID_State = '.$id_state.'
+				WHERE loan.ID_Loan = '.$id_loan;
+			if (!mysql_query($sql))
+			{
+				die('Error: ' . mysql_error());
+				return 0;
+			}
+			return 1;
+	}
 
 ?>
--- materimacBDD.sql
+++ materimacBDD.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Mar 27 Mai 2014 à 17:03
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Client: localhost
+-- Généré le: Mer 28 Mai 2014 à 07:04
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.3.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `materimac`
+-- Base de données: `materimacbdd`
 --
 
 -- --------------------------------------------------------
@@ -29,8 +29,19 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `category` (
   `ID_Category` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(64) NOT NULL,
+  `Color` varchar(36) NOT NULL,
   PRIMARY KEY (`ID_Category`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `category`
+--
+
+INSERT INTO `category` (`ID_Category`, `label`, `Color`) VALUES
+(1, 'photo-video', 'blue'),
+(2, 'son', 'red'),
+(3, 'lumiere', 'yellow'),
+(4, 'accessoires', 'green');
 
 -- --------------------------------------------------------
 
@@ -51,6 +62,17 @@ CREATE TABLE IF NOT EXISTS `item` (
   KEY `ID_Type` (`ID_Type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `item`
+--
+
+INSERT INTO `item` (`ID_item`, `label`, `description`, `ID_Kit`, `ID_Type`, `brand`, `model`) VALUES
+('1', 'appareil photo', 'appareil photo', 1, 4, 'Canon', '600D'),
+('2', 'oculus rift', 'oculus rift', 2, 6, 'Autre', '1'),
+('3', 'projecteur', 'projecteur', 3, 7, 'Autre', 'none'),
+('4', 'Mbox', 'mbox', 4, 8, 'Autre', 'none'),
+('5', 'objectif', 'objectif', 1, 5, 'Canon', '18-135mm');
+
 -- --------------------------------------------------------
 
 --
@@ -62,7 +84,17 @@ CREATE TABLE IF NOT EXISTS `kit` (
   `label` varchar(128) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`ID_Kit`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `kit`
+--
+
+INSERT INTO `kit` (`ID_Kit`, `label`, `description`) VALUES
+(1, 'Kit Appareil photo', 'Appareil photo '),
+(2, 'Kit Oculus', 'Oculus rift'),
+(3, 'Kit Projecteur', 'projecteur'),
+(4, 'Kit Mbox', 'mbox');
 
 -- --------------------------------------------------------
 
@@ -79,6 +111,17 @@ CREATE TABLE IF NOT EXISTS `lending` (
   KEY `ID_Kit` (`ID_Kit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `lending`
+--
+
+INSERT INTO `lending` (`ID_User`, `ID_Loan`, `ID_Kit`) VALUES
+(2, 3, 1),
+(2, 3, 2),
+(2, 4, 3),
+(2, 4, 4),
+(2, 5, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -91,9 +134,44 @@ CREATE TABLE IF NOT EXISTS `loan` (
   `project_name` varchar(32) NOT NULL,
   `loan_date` datetime NOT NULL COMMENT 'date du pret',
   `due_date` datetime NOT NULL COMMENT 'date du rendu',
-  `collaborators` text NOT NULL COMMENT 'autres membres',
-  PRIMARY KEY (`ID_Loan`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `collaborators` text COMMENT 'autres membres',
+  `ID_State` int(11) NOT NULL,
+  PRIMARY KEY (`ID_Loan`),
+  KEY `ID_State` (`ID_State`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `loan`
+--
+
+INSERT INTO `loan` (`ID_Loan`, `tutor`, `project_name`, `loan_date`, `due_date`, `collaborators`, `ID_State`) VALUES
+(3, 'Vincent Nozick', 'lol', '2014-05-08 00:00:00', '2014-05-23 00:00:00', NULL, 1),
+(4, 'Mathieu Constant', 'lolilol', '2014-06-11 00:00:00', '2014-06-19 00:00:00', NULL, 3),
+(5, 'Biri', 'troll', '2014-06-18 00:00:00', '2014-06-25 00:00:00', NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `state`
+--
+
+CREATE TABLE IF NOT EXISTS `state` (
+  `ID_State` int(11) NOT NULL AUTO_INCREMENT,
+  `name_state` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID_State`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `state`
+--
+
+INSERT INTO `state` (`ID_State`, `name_state`) VALUES
+(1, 'emprunté'),
+(2, 'en cours de validation'),
+(3, 'validé'),
+(4, 'annulé'),
+(5, 'refusé'),
+(6, 'retourné');
 
 -- --------------------------------------------------------
 
@@ -108,7 +186,18 @@ CREATE TABLE IF NOT EXISTS `type` (
   PRIMARY KEY (`ID_Type`),
   KEY `ID_Category` (`ID_Category`),
   KEY `ID_Category_2` (`ID_Category`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+--
+-- Contenu de la table `type`
+--
+
+INSERT INTO `type` (`ID_Type`, `label`, `ID_Category`) VALUES
+(4, 'photo', 1),
+(5, 'objectif', 1),
+(6, 'oculus rift', 1),
+(7, 'projecteur', 1),
+(8, 'mbox', 2);
 
 -- --------------------------------------------------------
 
@@ -125,7 +214,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `e_mail` varchar(32) NOT NULL,
   `grade` varchar(32) NOT NULL,
   PRIMARY KEY (`ID_User`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`ID_User`, `ID_Student`, `login`, `lastname`, `firstname`, `e_mail`, `grade`) VALUES
+(1, 0, 'admin', 'admin', 'admin', 'admin@gmail.com', 'nograde'),
+(2, 82431, 'jpo', 'po', 'julie', 'pojulie07@gmail.com', 'imac2');
 
 --
 -- Contraintes pour les tables exportées
@@ -135,16 +232,16 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Contraintes pour la table `item`
 --
 ALTER TABLE `item`
-  ADD CONSTRAINT `item_ibfk_2` FOREIGN KEY (`ID_Type`) REFERENCES `type` (`ID_Type`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`ID_Kit`) REFERENCES `kit` (`ID_Kit`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`ID_Kit`) REFERENCES `kit` (`ID_Kit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `item_ibfk_2` FOREIGN KEY (`ID_Type`) REFERENCES `type` (`ID_Type`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `lending`
 --
 ALTER TABLE `lending`
-  ADD CONSTRAINT `lending_ibfk_4` FOREIGN KEY (`ID_Loan`) REFERENCES `loan` (`ID_Loan`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `lending_ibfk_1` FOREIGN KEY (`ID_User`) REFERENCES `user` (`ID_User`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lending_ibfk_3` FOREIGN KEY (`ID_Kit`) REFERENCES `kit` (`ID_Kit`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `lending_ibfk_3` FOREIGN KEY (`ID_Kit`) REFERENCES `kit` (`ID_Kit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lending_ibfk_4` FOREIGN KEY (`ID_Loan`) REFERENCES `loan` (`ID_Loan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `type`
