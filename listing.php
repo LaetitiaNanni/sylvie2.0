6fe9f1e289752fe3f83cb7b80b3f96c87ed0e51c
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-05-28 12:47:55 +0200

    listing dynamique

--- listing.php
+++ listing.php
@@ -1,54 +1,33 @@
-<div class="col-md-6 item">
-	<div class="row">
-		<div class="col-xs-4">
-			<img src="http://fakeimg.pl/100/">
-		</div>
-		<div class="col-xs-8">
-			<p>nom</p>
-			<a href="">Calendrier</a>
-			<a href="">Ajouter à la liste</a>
 
-		</div>
-	</div>
-</div>
-<div class="col-md-6 item">
-	<div class="row">
-		<div class="col-xs-4">
-			<img src="http://fakeimg.pl/100/">
-		</div>
-		<div class="col-xs-8">
-			<p>nom</p>
-			<a href="">Calendrier</a>
-		</div>
-	</div>
-</div>
-<div class="col-md-6 item">
-	<div class="row">
-		<div class="col-xs-4">
-			<img src="http://fakeimg.pl/100/">
-		</div>
-		<div class="col-xs-8">
-			<p>nom</p>
-			<a href="">Calendrier</a>
-		</div>
-	</div>
-</div>
 <?php
+	require_once('conf/top.php');
+	require_once('all-variables.php');
 
 	$allKits = getAllKits();
 	while($kit = mysql_fetch_object($allKits)) {
+		echo "<div class=\"col-md-6 item\">
+				<div class=\"row\">
+					<div class=\"col-xs-4\">
+								<img src=\"http://fakeimg.pl/100/\">
+					</div>				
+		";
+		echo "<div class=\"col-xs-8\">";
+			echo ($kit-> label);
+			echo ($kit-> ID_Kit);
 
-		echo ($kit-> label);
-		echo ($kit-> ID_Kit);
-
-		$materiaux = getItemFromKit($kit-> ID_Kit);
-
-		while($matos = mysql_fetch_object($materiaux)) {
-			echo ($matos-> label);
-			echo ($matos-> description);
-			echo ($matos-> brand);
-			echo ($matos-> model);
-		}
+			$materiaux = getItemFromKit($kit-> ID_Kit);
+			echo "<ul>";
+				while($matos = mysql_fetch_object($materiaux)) {
+					
+						echo ("<li>"+($matos-> label)+"</li>");
+						echo ("<li>"+($matos-> brand)+"</li>");
+						echo ("<li>"+($matos-> model)+"</li>");
+					
+				}
+			echo "</ul>";
+			echo '<a href="">Calendrier</a>';
+			echo '<a href="">Ajouter à la liste</a>';
+		echo "</div> </div> </div>";
 	}
 
 ?>
